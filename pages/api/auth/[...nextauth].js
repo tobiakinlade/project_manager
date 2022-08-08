9813542fd0ae0f63def8080687fd59ab6ffc5dad
tobiakinlade <tobi.akinlade@yahoo.com>
2022-08-08 13:37:54 +0100

    auth and DB setup

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,39 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from 'lib/prisma'
+import EmailProvider from 'next-auth/providers/email'
+import NextAuth from 'next-auth'
+
+export default NextAuth({
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+
+  database: process.env.DATABASE_URL,
+  secret: process.env.SECRET,
+
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+
+  debug: true,
+  adapter: PrismaAdapter(prisma),
+
+  callbacks: {
+    session: async ({ session, user }) => {
+      session.user.id = user.id
+      session.user.isSubscriber = user.isSubscriber
+      return Promise.resolve(session)
+    },
+  },
+})
