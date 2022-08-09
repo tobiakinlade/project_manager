bf3f990d92b69aede4db4b10cea19efb9ca82128
tobiakinlade <tobi.akinlade@yahoo.com>
2022-08-09 12:54:46 +0100

    add projects to DB and displaying from DB

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,13 @@
-import { useSession } from 'next-auth/react'
+import { getSession, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
+import { getProjects } from 'lib/data'
+import prisma from 'lib/prisma'
 
-export default function Dashboard() {
+export default function Dashboard({ projects }) {
   const router = useRouter()
+  const [name, setName] = useState('')
 
   const { data: session, status } = useSession()
 
@@ -33,28 +36,62 @@ export default function Dashboard() {
 
       <div className='text-center'>
         <h1 className='mt-10 font-extrabold text-2xl'>Project Manager</h1>
+        <form
+          className='mt-10 flex flex-row justify-center'
+          onSubmit={async (e) => {
+            e.preventDefault()
+            await fetch('/api/project', {
+              body: JSON.stringify({
+                name,
+              }),
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              method: 'POST',
+            })
+            router.reload()
+          }}
+        >
+          <input
+            onChange={(e) => setName(e.target.value)}
+            className='border p-2 text-black outline-none'
+            required
+            placeholder='New project'
+          />
+          <button
+            disabled={name ? false : true}
+            className={`border px-8 py-2 font-bold ${
+              name ? '' : 'cursor-not-allowed text-gray-400 border-gray-400'
+            }`}
+          >
+            Add
+          </button>
+        </form>
         <div className='grid sm:grid-cols-2'>
-          <div>
-            <h2 className='mt-10 font-bold'>Project #1</h2>
+          {projects.map((project) => (
+            <div>
+              <h2 className='mt-10 font-bold'>Project #2</h2>
 
-            <ol className='mt-4 list-inside list-decimal'>
-              <li>TODO 1</li>
-              <li>TODO 2</li>
-              <li>TODO 3</li>
-            </ol>
-          </div>
-
-          <div>
-            <h2 className='mt-10 font-bold'>Project #2</h2>
-
-            <ol className='mt-4 list-inside list-decimal'>
-              <li>TODO 1</li>
-              <li>TODO 2</li>
-              <li>TODO 3</li>
-            </ol>
-          </div>
+              <ol className='mt-4 list-inside list-decimal'>
+                <li>TODO 1</li>
+                <li>TODO 2</li>
+                <li>TODO 3</li>
+              </ol>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  const projects = await getProjects(prisma, session?.user.id)
+
+  return {
+    props: {
+      projects,
+    },
+  }
 }
